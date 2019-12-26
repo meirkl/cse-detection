8637b8a7c8203da687f3d783bcd4f59478119f57
Keller, Meir <meir.keller@intel.com>
2019-12-26 18:49:54 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { parseString } from 'xml2js';
+import { Table } from 'react-bootstrap';
 
 function App() {
+  const [file, setFile] = useState(null);
+  const [xmlJson, setXmlJson] = useState('');
+  useEffect(() => {
+    if (file) {
+      const reader = new FileReader();
+      reader.readAsText(file, 'utf-8');
+      reader.onload = () => {
+        parseString(reader.result, (err, result) => {
+          const { System: data } = result;
+          setXmlJson(data);
+        });
+      };
+      reader.onerror = () => {
+        console.error(`Unable to read file: ${reader.error}`);
+      };
+    }
+  }, [file]);
+
+  const displayContent = data => {
+    if (typeof data === 'string' || data instanceof String) {
+      return data;
+    }
+    if (Array.isArray(data)) {
+      if (typeof data[0] === 'object' || data[0] instanceof Object) {
+        return Object.keys(data[0]).map(key => {
+          return (
+            <p>
+              <strong>{key}:&nbsp;</strong>
+              {data[0][key]}
+            </p>
+          );
+        });
+      }
+      return data[0];
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <h1>CSE Detection</h1>
+      <div className="form-group">
+        <label htmlFor="file-input">Detection XML file</label>
+        <input
+          id="file-input"
+          className="form-control-file"
+          type="file"
+          onChange={e => setFile(e.target.files[0])}
+        />
+      </div>
+      <div>
+        {xmlJson && (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Application_Name</th>
+                <th>Application_Version</th>
+                <th>Computer_Name</th>
+                <th>Scan_Date</th>
+                <th>Hardware_Inventory</th>
+                <th>ME_Firmware_Information</th>
+                <th>System_Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>{displayContent(xmlJson.Application_Name)}</td>
+                <td>{displayContent(xmlJson.Application_Version)}</td>
+                <td>{displayContent(xmlJson.Computer_Name)}</td>
+                <td>{displayContent(xmlJson.Scan_Date)}</td>
+                <td>{displayContent(xmlJson.Hardware_Inventory)}</td>
+                <td>{displayContent(xmlJson.ME_Firmware_Information)}</td>
+                <td>{displayContent(xmlJson.System_Status)}</td>
+              </tr>
+            </tbody>
+          </Table>
+        )}
+      </div>
     </div>
   );
 }
