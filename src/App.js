b0afd7425d575df31bc23567fefabd87b6ea2f6a
Meir <mrzkeller@gmail.com>
2019-12-29 00:28:30 +0200

    Added drag n drop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 import { parseString } from 'xml2js';
-import { Table } from 'react-bootstrap';
+import DropZone from './DropZone';
 
-function App() {
-  const [file, setFile] = useState(null);
+const App = () => {
+  const [fileName, setFileName] = useState('');
   const [xmlJson, setXmlJson] = useState('');
-  useEffect(() => {
-    if (file) {
-      const reader = new FileReader();
-      reader.readAsText(file, 'utf-8');
-      reader.onload = () => {
-        parseString(reader.result, (err, result) => {
-          const { System: data } = result;
-          setXmlJson(data);
-        });
-      };
-      reader.onerror = () => {
-        console.error(`Unable to read file: ${reader.error}`);
-      };
-    }
-  }, [file]);
 
   const displayContent = data => {
     if (typeof data === 'string' || data instanceof String) {
@@ -40,50 +25,71 @@ function App() {
     }
   };
 
+  const onDrop = useCallback(([file]) => {
+    if (file) {
+      setFileName(file.name);
+      const reader = new FileReader();
+      reader.readAsText(file, 'utf-8');
+      reader.onload = () => {
+        parseString(reader.result, (_, result) => {
+          const { System: data } = result;
+          setXmlJson(data);
+        });
+      };
+      reader.onerror = () => {
+        console.error(`Unable to read file: ${reader.error}`);
+      };
+    }
+  }, []);
+
+  const reset = () => {
+    setFileName('');
+    setXmlJson('');
+  };
+
   return (
     <div className="container-fluid">
-      <h1>CSE Detection</h1>
-      <div className="form-group">
-        <label htmlFor="file-input">Detection XML file</label>
-        <input
-          id="file-input"
-          className="form-control-file"
-          type="file"
-          onChange={e => setFile(e.target.files[0])}
-        />
-      </div>
+      <h1 className="mt-3">CSE Detection</h1>
+      <DropZone onDrop={onDrop} />
       <div>
         {xmlJson && (
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Application_Name</th>
-                <th>Application_Version</th>
-                <th>Computer_Name</th>
-                <th>Scan_Date</th>
-                <th>Hardware_Inventory</th>
-                <th>ME_Firmware_Information</th>
-                <th>System_Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>{displayContent(xmlJson.Application_Name)}</td>
-                <td>{displayContent(xmlJson.Application_Version)}</td>
-                <td>{displayContent(xmlJson.Computer_Name)}</td>
-                <td>{displayContent(xmlJson.Scan_Date)}</td>
-                <td>{displayContent(xmlJson.Hardware_Inventory)}</td>
-                <td>{displayContent(xmlJson.ME_Firmware_Information)}</td>
-                <td>{displayContent(xmlJson.System_Status)}</td>
-              </tr>
-            </tbody>
-          </Table>
+          <>
+            <div className="d-flex mt-3">
+              <h3>{fileName}</h3>
+              <button className="btn btn-warning align-self-end ml-2" onClick={reset}>
+                <span className="font-weight-bold">&#8635;</span>
+              </button>
+            </div>
+            <table className="mt-3 table table-bordered">
+              <caption>CSE Detection xml data</caption>
+              <thead>
+                <tr>
+                  <th>Application_Name</th>
+                  <th>Application_Version</th>
+                  <th>Computer_Name</th>
+                  <th>Scan_Date</th>
+                  <th>Hardware_Inventory</th>
+                  <th>ME_Firmware_Information</th>
+                  <th>System_Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{displayContent(xmlJson.Application_Name)}</td>
+                  <td>{displayContent(xmlJson.Application_Version)}</td>
+                  <td>{displayContent(xmlJson.Computer_Name)}</td>
+                  <td>{displayContent(xmlJson.Scan_Date)}</td>
+                  <td>{displayContent(xmlJson.Hardware_Inventory)}</td>
+                  <td>{displayContent(xmlJson.ME_Firmware_Information)}</td>
+                  <td>{displayContent(xmlJson.System_Status)}</td>
+                </tr>
+              </tbody>
+            </table>
+          </>
         )}
       </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/DropZone.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useDropzone } from 'react-dropzone';
+import styled from 'styled-components';
+
+const getColor = props => {
+  if (props.isDragAccept) {
+    return '#00e676';
+  }
+  if (props.isDragReject) {
+    return '#ff1744';
+  }
+  if (props.isDragActive) {
+    return '#2196f3';
+  }
+  return '#eeeeee';
+};
+
+const Container = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  border-width: 2px;
+  border-radius: 2px;
+  border-color: ${props => getColor(props)};
+  border-style: dashed;
+  background-color: #fafafa;
+  color: #bdbdbd;
+  outline: none;
+  transition: border 0.24s ease-in-out;
+`;
+
+const DropZone = props => {
+  const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } = useDropzone({
+    accept: ['application/xml', 'text/xml'],
+    multiple: false,
+    onDrop: props.onDrop
+  });
+
+  return (
+    <div className="container mt-3">
+      {console.log(getRootProps({ isDragActive, isDragAccept, isDragReject }))}
+      <Container {...getRootProps({ isDragActive, isDragAccept, isDragReject })}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop CSE Detection XML</p>
+      </Container>
+    </div>
+  );
+};
+
+export default DropZone;
