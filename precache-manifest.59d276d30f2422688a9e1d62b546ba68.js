869bcd18ae0dab828c74cef2b2149886d4089078
Keller, Meir <meir.keller@intel.com>
2019-12-30 08:34:05 +0200

    Updates

---
+++ precache-manifest.59d276d30f2422688a9e1d62b546ba68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec94de84278b69973a5b4938cb3c383",
+    "url": "/cse-detection/index.html"
+  },
+  {
+    "revision": "da873a42742cc65afe60",
+    "url": "/cse-detection/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "da873a42742cc65afe60",
+    "url": "/cse-detection/static/js/2.0b954a33.chunk.js"
+  },
+  {
+    "revision": "18f4d45f7f77c388f5b6a6c66c21d8d1",
+    "url": "/cse-detection/static/js/2.0b954a33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d0f39c7dca8c1258fa5c",
+    "url": "/cse-detection/static/js/main.b29dc207.chunk.js"
+  },
+  {
+    "revision": "3681728754ba89fe5576",
+    "url": "/cse-detection/static/js/runtime-main.9ab1609b.js"
+  }
+]);
